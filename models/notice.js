c0d0114fbd79629d0760ec5af3c6fb4a9d9d1f65
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-03-18 01:10:00 +0200

    321

--- models/notice.js
+++ models/notice.js
@@ -2,6 +2,7 @@ const { noticeService } = require("../services/noticeService");
 
 const getNotice = async (_, res) => {
   const notice = await noticeService();
+  console.log(notice);
   res.json(notice);
 };
 
--- schemas/noticeModel.js
+++ schemas/noticeModel.js
@@ -1,22 +1,21 @@
 const mongoose = require("mongoose");
 
-const noticeSchema = new mongoose.Schema(
-  {
-    topic: {
-      type: String,
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: String,
-      required: true,
-    },
-  },
-  { versionKey: false }
-);
+const noticeSchema = new mongoose.Schema();
+//   {
+//     topic: {
+//       type: String,
+//       required: true,
+//     },
+//     text: {
+//       type: String,
+//       required: true,
+//     },
+//     createdAt: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { versionKey: false }
 
 const Notice = mongoose.model("notice", noticeSchema);
 
