639c8ea808cb6f10545642a32d1e31ecec236cb3
Olena <kryvtsovaolena@gmail.com>
2023-03-18 08:46:33 +0200

    register
    add hashPassword

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post(
   "/register",
-  // validation(joiUserSchema),
+  validation(joiUserSchema),
   asyncWrapper(controller.register)
 );
 module.exports = router;
