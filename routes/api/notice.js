7452333a7ee460d558604170186fa0bf8e2e1ce0
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-18 11:06:12 +0200

    Update notice.js

--- routes/api/notice.js
+++ routes/api/notice.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const addNotice = require("../../services/notices/addNotice");
 const { controllerWrapper } = require("../../helpers/apiHelpers");
+const { addNotice, getAllNotices } = require("../../models/notices");
 
 router.post("/create", controllerWrapper(addNotice));
+router.get("/", controllerWrapper(getAllNotices));
+
+//router.get(
+//  "/category/:category",
+//  isValidCategory,
+//  controllerWrapper(getNoticesByCategory)
+//);
 
 module.exports = router;
