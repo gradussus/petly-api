0c70fb9314ec3d6c041e8086fef5c5495b25b599
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-18 11:05:51 +0200

    Create addNotice.js

---
+++ models/notices/addNotice.js
@@ -0,0 +1,16 @@
+const { Notice } = require("../../schemas/noticeModel");
+
+const addNotice = async (req, res) => {
+  const newNotice = await Notice.create({ ...req.body });
+  //let newNotice = await Notice.create({ ...req.body, owner: req.userId });
+  //newNotice = await newNotice.populate("owner", "email phone");
+  console.log(newNotice);
+
+  res.status(201).json({
+    code: 201,
+    status: "success",
+    data: newNotice,
+  });
+};
+
+module.exports = addNotice;
