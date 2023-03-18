ef37b1a1164cb0ae35e4096dff4d595de629970b
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-18 15:40:34 +0200

    Create getNoticesBySearch.js

---
+++ models/notices/getNoticesBySearch.js
@@ -0,0 +1,27 @@
+const { Notice } = require("../../schemas/noticeModel");
+
+const getNoticesBySearch = async (req, res) => {
+  const { categoryName, qwery } = req.params;
+
+  const notices = await Notice.find({
+    category: categoryName,
+    title: { $regex: qwery, $options: "i" },
+  }).sort({
+    createdAt: -1,
+  });
+
+  if (notices.length === 0) {
+    res.json({
+      code: 404,
+      message: "Not found",
+    });
+  }
+
+  res.json({
+    code: 200,
+    status: "success",
+    data: notices,
+  });
+};
+
+module.exports = getNoticesBySearch;
