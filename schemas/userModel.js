7b709c5c5bb3f58759a8699053ba8b56ab407741
Olena <kryvtsovaolena@gmail.com>
2023-03-19 20:30:14 +0200

    fix userschema

--- schemas/userModel.js
+++ schemas/userModel.js
@@ -28,10 +28,7 @@ const userSchema = Schema(
       type: String,
       required: [true, "Set mobile for user"],
     },
-    birthData: {
-      type: String,
-      default: "00.00.0000",
-    },
+
     token: {
       type: String,
       default: null,
@@ -58,18 +55,17 @@ userSchema.methods.matchPassword = async function (password) {
     throw new Error(error);
   }
 };
-const nameRegexp = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+const nameRegexp = /^[a-zA-Zа-яА-ЯіІїЇґҐ]+(?: [a-zA-Zа-яА-ЯіІїЇґҐ]+)*$/;
 const emailRegexp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-const passwordRegexp =
-  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[#?!@$%^&*-_])\S{7,32}$/;
+const passwordRegexp = /^[a-zA-Z0-9а-яА-Я]+$/;
 const phoneRegexp = /^\+380\d{9}$/;
-const cityRegexp = /([A-Za-z]+(?: [A-Za-z]+)*),? ([A-Za-z]{2})/;
+const cityRegexp = /^[а-яёіїєА-ЯЁІЇЄA-Za-z]+,?\s[а-яёіїєА-ЯЁІЇЄA-Za-z]+$/;
 
 const joiUserSchema = Joi.object({
   name: Joi.string().pattern(nameRegexp).required(),
   password: Joi.string().pattern(passwordRegexp).min(7).max(32).required(),
   email: Joi.string().pattern(emailRegexp).required(),
-  city: Joi.string().pattern(cityRegexp).required(),
+  city: Joi.string().pattern(cityRegexp).min(2).required(),
   phone: Joi.string().pattern(phoneRegexp).required(),
 });
 
