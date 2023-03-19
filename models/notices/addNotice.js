9afcf622fc237d16d8919c7f72afe57af3db9b37
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-19 16:01:01 +0200

    Update addNotice.js

--- models/notices/addNotice.js
+++ models/notices/addNotice.js
@@ -1,15 +1,29 @@
 const { Notice } = require("../../schemas/noticeModel");
 
 const addNotice = async (req, res) => {
-  const newNotice = await Notice.create({ ...req.body });
-  //let newNotice = await Notice.create({ ...req.body, owner: req.userId });
-  //newNotice = await newNotice.populate("owner", "email phone");
-  console.log(newNotice);
+  //const newNotice = await Notice.create({ ...req.body });
+
+  const { categoryName } = req.params;
+  const { body } = req;
+  const { _id } = req.user;
+
+  console.log(req);
+  console.log(_id);
+
+  const newNotice = await Notice.create({
+    ...body,
+    photoURL: req?.file?.path,
+    category: categoryName,
+    owner: _id,
+  });
+
+  //const notice = await newNotice.populate("owner", "email, phone");
 
   res.status(201).json({
     code: 201,
     status: "success",
     data: newNotice,
+    //data: notice,
   });
 };
 
