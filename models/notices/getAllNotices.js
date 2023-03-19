a88bc426afa88043760d121e437baee1e07d4e67
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-19 17:16:55 +0200

    Update getAllNotices.js

--- models/notices/getAllNotices.js
+++ models/notices/getAllNotices.js
@@ -3,6 +3,13 @@ const { Notice } = require("../../schemas/noticeModel");
 const getAllNotices = async (req, res) => {
   const notices = await Notice.find().sort({ createdAt: -1 });
 
+  if (notices.length === 0) {
+    res.json({
+      code: 404,
+      message: "Not found",
+    });
+  }
+
   // res.json({
   //   code: 200,
   //   status: "success",
