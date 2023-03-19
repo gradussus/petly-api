1d0b865f96cdf42ca336ab614211784a2fe81ae9
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-19 09:16:51 +0200

    Update index.js

--- models/notices/index.js
+++ models/notices/index.js
@@ -2,13 +2,16 @@ const getAllNotices = require("./getAllNotices");
 
 const addNotice = require("./addNotice");
 
-const getNoticesByCategory = require("./getNoticesByCategory.js");
+const getNoticesByCategory = require("./getNoticesByCategory");
 
 const getNoticesBySearch = require("./getNoticesBySearch");
+
+const getNoticeById = require("./getNoticeById");
 
 module.exports = {
   addNotice,
   getAllNotices,
   getNoticesByCategory,
   getNoticesBySearch,
+  getNoticeById,
 };
