e238761119d143a98ca30b2346f91f9c82dc2fb5
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-19 17:07:24 +0200

    Update getNoticesBySearch.js

--- models/notices/getNoticesBySearch.js
+++ models/notices/getNoticesBySearch.js
@@ -17,11 +17,13 @@ const getNoticesBySearch = async (req, res) => {
     });
   }
 
-  res.json({
-    code: 200,
-    status: "success",
-    data: notices,
-  });
+  // res.json({
+  //   code: 200,
+  //   status: "success",
+  //   data: notices,
+  // });
+
+  res.status(200).json(notices);
 };
 
 module.exports = getNoticesBySearch;
