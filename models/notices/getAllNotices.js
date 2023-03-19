0d4b47eff6a8a558a3727bb119fc0a9b25d60300
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-19 17:07:11 +0200

    Update getAllNotices.js

--- models/notices/getAllNotices.js
+++ models/notices/getAllNotices.js
@@ -3,11 +3,12 @@ const { Notice } = require("../../schemas/noticeModel");
 const getAllNotices = async (req, res) => {
   const notices = await Notice.find().sort({ createdAt: -1 });
 
-  res.json({
-    code: 200,
-    status: "success",
-    data: notices,
-  });
+  // res.json({
+  //   code: 200,
+  //   status: "success",
+  //   data: notices,
+  // });
+  res.status(200).json(notices);
 };
 
 module.exports = getAllNotices;
