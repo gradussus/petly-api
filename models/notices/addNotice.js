c20dcf5f917ab751f684d915b3d4b8db3f2f9f0c
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-20 15:17:15 +0200

    Update addNotice.js

--- models/notices/addNotice.js
+++ models/notices/addNotice.js
@@ -1,14 +1,13 @@
 const { Notice } = require("../../schemas/noticeModel");
 
 const addNotice = async (req, res) => {
-  const { categoryName } = req.params;
   const { body } = req;
   const { _id } = req.user;
 
   const newNotice = await Notice.create({
     ...body,
     photoURL: req?.file?.path,
-    category: categoryName,
+
     owner: _id,
   });
 
