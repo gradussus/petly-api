d550fb80e375cdcd1a793fa049a89c912a32b6ea
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-21 21:40:46 +0200

    Create friends.js

---
+++ models/friends/friends.js
@@ -0,0 +1,15 @@
+const { friendsService } = require("../../services/friendsService");
+
+const getFriends = async (_, res) => {
+  const friends = await friendsService();
+
+  res.status(200).json(friends);
+
+  // res.status(200).json({
+  //   code: 200,
+  //   status: "success",
+  //   data: friends,
+  // });
+};
+
+module.exports = { getFriends };
