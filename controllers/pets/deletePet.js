9d0d40456c0eb0d6b2d64b13d1d31a4726d34487
Olena <kryvtsovaolena@gmail.com>
2023-03-22 14:51:47 +0200

    pet delete

--- controllers/pets/deletePet.js
+++ controllers/pets/deletePet.js
@@ -3,23 +3,19 @@ const { NotFound } = require("http-errors");
 const { Pet } = require("../../schemas/petModel");
 
 const deletePet = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    const { id } = req.params;
+  const { _id } = req.user;
+  const { id } = req.params;
 
-    const petToDelete = await Pet.findByIdAndRemove({
-      _id: id,
-      owner: { _id },
-    });
-    if (!petToDelete) {
-      throw new NotFound(`Contact with id=${id} not found`);
-    }
-    res.status(200).json({
-      data: { petToDelete },
-    });
-  } catch (error) {
-    next(error);
+  const petToDelete = await Pet.findByIdAndRemove({
+    _id: id,
+    owner: { _id },
+  });
+  if (!petToDelete) {
+    throw new NotFound(`Contact with id=${id} not found`);
   }
+  res.status(200).json({
+    data: { petToDelete },
+  });
 };
 
 module.exports = deletePet;
