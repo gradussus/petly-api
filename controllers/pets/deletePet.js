693f1045f8098a04790a9513d8a2de860ddd6752
Olena <kryvtsovaolena@gmail.com>
2023-03-22 14:50:33 +0200

    pet deleted

---
+++ controllers/pets/deletePet.js
@@ -0,0 +1,25 @@
+const { NotFound } = require("http-errors");
+
+const { Pet } = require("../../schemas/petModel");
+
+const deletePet = async (req, res, next) => {
+  try {
+    const { _id } = req.user;
+    const { id } = req.params;
+
+    const petToDelete = await Pet.findByIdAndRemove({
+      _id: id,
+      owner: { _id },
+    });
+    if (!petToDelete) {
+      throw new NotFound(`Contact with id=${id} not found`);
+    }
+    res.status(200).json({
+      data: { petToDelete },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = deletePet;
