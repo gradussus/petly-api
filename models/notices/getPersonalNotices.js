cac95758399d1b522d9eb01fc49dd098ffa0ec8b
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-22 11:03:26 +0200

    Update getPersonalNotices.js

--- models/notices/getPersonalNotices.js
+++ models/notices/getPersonalNotices.js
@@ -9,15 +9,8 @@ const getPersonalNotices = async (req, res) => {
     createdAt: -1,
   });
 
-  //   if (notices.length === 0) {
-  //     res.status(404).json({
-  //       message: "Not found",
-  //     });
-  //     }
-
   if (notices.length === 0) {
     res.json({
-      code: 404,
       message: "Not found",
     });
   }
