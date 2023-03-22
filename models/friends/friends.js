fad2c1fe108c57352218dec5ec704072537d3500
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-22 11:02:15 +0200

    Update friends.js

--- models/friends/friends.js
+++ models/friends/friends.js
@@ -4,12 +4,6 @@ const getFriends = async (_, res) => {
   const friends = await friendsService();
 
   res.status(200).json(friends);
-
-  // res.status(200).json({
-  //   code: 200,
-  //   status: "success",
-  //   data: friends,
-  // });
 };
 
 module.exports = { getFriends };
