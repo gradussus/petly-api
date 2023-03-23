79174237ffd1762ccb078cc4b7c5b2dfd5623c84
Olena <kryvtsovaolena@gmail.com>
2023-03-24 00:32:49 +0200

    current user+keys for cloudinary

--- controllers/auth/currentUser.js
+++ controllers/auth/currentUser.js
@@ -1,5 +1,6 @@
 const currentUser = async (req, res, next) => {
-  const { name, email, city, avatarURL, phone, birthDate } = req.user;
+  const { name, email, city, avatarURL, phone, birthDate, favoriteList } =
+    req.user;
 
   res.status(200).json({
     user: {
@@ -9,6 +10,7 @@ const currentUser = async (req, res, next) => {
       avatarURL,
       phone,
       birthDate,
+      favoriteList,
     },
   });
 };
