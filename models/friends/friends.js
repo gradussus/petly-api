cbb953d9d2f9794db217bb86c801de3b338e59ab
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-23 22:13:03 +0200

    Delete friends.js

--- models/friends/friends.js
+++
@@ -1,9 +0,0 @@
-const { friendsService } = require("../../services/friendsService");
-
-const getFriends = async (_, res) => {
-  const friends = await friendsService();
-
-  res.status(200).json(friends);
-};
-
-module.exports = { getFriends };
