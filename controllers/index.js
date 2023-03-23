edea44d94bf5b00a09203e3cd026ed9d08270921
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-23 22:11:54 +0200

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,7 @@
 const auth = require("./auth");
 const pets = require("./pets");
-module.exports = { auth, pets };
+const friends = require("./friends");
+const news = require("./news");
+const notices = require("./notices");
+
+module.exports = { auth, pets, friends, news, notices };
