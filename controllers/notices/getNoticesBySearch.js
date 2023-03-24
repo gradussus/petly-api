cd0c9d73aa800b7721b103b2b5af54e09d97c8d3
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-24 10:09:34 +0200

    Update getNoticesBySearch.js

--- controllers/notices/getNoticesBySearch.js
+++ controllers/notices/getNoticesBySearch.js
@@ -2,29 +2,11 @@ const { Notice } = require("../../schemas/noticeModel");
 
 // пошук по слову і категорії
 
-//const getNoticesBySearch = async (req, res) => {
-//   const { categoryName, qwery } = req.params;
-
-//   const notices = await Notice.find({
-//     category: categoryName,
-//     title: { $regex: qwery, $options: "i" },
-//   }).sort({
-//     createdAt: -1,
-//   });
-
-//   // if (notices.length === 0) {
-//   //   res.json({
-//   //     message: "Not found",
-//   //   });
-//   // }
-
-//   res.status(200).json(notices);
-// };
-
 const getNoticesBySearch = async (req, res) => {
-  const { qwery } = req.params;
+  const { categoryName, qwery } = req.params;
 
   const notices = await Notice.find({
+    category: categoryName,
     title: { $regex: qwery, $options: "i" },
   }).sort({
     createdAt: -1,
@@ -38,5 +20,17 @@ const getNoticesBySearch = async (req, res) => {
 
   res.status(200).json(notices);
 };
+
+// const getNoticesBySearch = async (req, res) => {
+//   const { qwery } = req.params;
+
+//   const notices = await Notice.find({
+//     title: { $regex: qwery, $options: "i" },
+//   }).sort({
+//     createdAt: -1,
+//   });
+
+//   res.status(200).json(notices);
+// };
 
 module.exports = getNoticesBySearch;
