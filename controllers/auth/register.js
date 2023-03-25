a4f36b07f9a0896d6c7df3b79caea49cb71d335b
Olena <kryvtsovaolena@gmail.com>
2023-03-25 11:33:26 +0200

    token for registration

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 const { User } = require("../../schemas/userModel");
 const gravatar = require("gravatar");
 
@@ -10,7 +12,7 @@ const register = async (req, res, next) => {
   }
 
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  console.log(hashPassword);
+
   await User.create({
     name,
     email,
@@ -19,11 +21,21 @@ const register = async (req, res, next) => {
     city,
     phone,
   });
+
+  const payload = {
+    id: User._id,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET);
+  await User.findByIdAndUpdate(User._id, { token });
+
   res.status(201).json({
     name,
     email,
+    avatarURL: gravatar.url(email),
     city,
     phone,
+    token,
   });
 };
 
