98bddf558d256d540866131e770bccddb1b82c2a
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-03-27 23:30:38 +0300

    start

--- controllers/google/auth.js
+++ controllers/google/auth.js
@@ -1,1 +1,32 @@
 const queryString = require("query-string");
+const axios = require("axios");
+const URL = require("url");
+
+const googleAuth = (req, res) => {
+  const stringifiedParams = queryString.stringify({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: `${process.env.BASE_URL}/google/google-redirect`,
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" "),
+    response_type: "code",
+    access_type: "offline",
+    prompt: "consent",
+  });
+
+  return res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
+  );
+};
+const googleRedirect = async (req, res) => {
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+
+  const urlObj = new URL(fullUrl);
+  const urlParams = queryString.parse(urlObj.search);
+};
+
+module.exports = {
+  googleAuth,
+  googleRedirect,
+};
