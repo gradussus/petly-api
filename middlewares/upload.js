08c2b639460df31bbe36d8ef03338f15f73ee479
Olena <kryvtsovaolena@gmail.com>
2023-03-27 21:36:23 +0300

    clean avatarUpdate

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,29 +1,29 @@
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 
-// const tempDir = path.join(__dirname, "../", "temp");
-const multerConfig = multer.diskStorage({
-  // destination: (req, file, cb) => {
-  //   cb(null, tempDir);
-  // },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  limits: {
-    fileSize: 2048000,
-  },
-});
+// // const tempDir = path.join(__dirname, "../", "temp");
+// const multerConfig = multer.diskStorage({
+//   // destination: (req, file, cb) => {
+//   //   cb(null, tempDir);
+//   // },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+//   limits: {
+//     fileSize: 2048000,
+//   },
+// });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-const upload = multer({
-  storage: multerConfig,
-  fileFilter: fileFilter,
-});
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
+// const upload = multer({
+//   storage: multerConfig,
+//   fileFilter: fileFilter,
+// });
 
-module.exports = upload;
+// module.exports = upload;
