2780db7531338f884606affc2f729439ff87ff2e
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-30 13:00:59 +0300

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,14 +1,12 @@
 const validation = require("./validation");
 const authenticate = require("./authenticate");
 const isValidId = require("./isValidId");
-const upload = require("./upload");
+
 const uploadCloud = require("./cloudinaryUploadMiddleware");
-const uploadImage = require("./cloudyupdate");
+
 module.exports = {
   validation,
   isValidId,
   authenticate,
   uploadCloud,
-  upload,
-  uploadImage,
 };
