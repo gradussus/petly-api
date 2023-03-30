0468a345ea0a4ef5d08f5177327ca99c10a4b9e9
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-30 11:37:30 +0300

    Update getNoticesByCategory.js

--- controllers/notices/getNoticesByCategory.js
+++ controllers/notices/getNoticesByCategory.js
@@ -3,32 +3,9 @@ const { Notice } = require("../../schemas/noticeModel");
 const getNoticesByCategory = async (req, res) => {
   const { categoryName } = req.params;
 
-  //const { page = 1, limit = 12 } = req.query;
-  //const skip = (page - 1) * limit;
-
-  //const notices = await Notice.find({ categoryName }, "-createdAt -updatedAt", {
-  // skip,
-  // limit,
-  //})
-  // .sort({ createdAt: -1 });
-
   const notices = await Notice.find({ category: categoryName }).sort({
     createdAt: -1,
   });
-
-  // if (notices.length === 0) {
-  //   res.status(404).json({
-  //     message: "Not found",
-  //   });
-  // }
-
-  //const total = await Notice.find({ category }).count();
-
-  // res.json({
-  //   data: notices,
-  //   totalPages: Math.ceil(total / limit),
-  //   page: page * 1,
-  // });
 
   res.json(notices);
 };
