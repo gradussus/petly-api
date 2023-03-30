5faf4ecd7efd39a665b30576184048b8d5950f7e
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-30 11:37:07 +0300

    Update news.js

--- routes/api/news.js
+++ routes/api/news.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const { controllerWrapper } = require("../../helpers/apiHelpers");
 const { getAllNews, getNewsBySearch } = require("../../controllers/news");
 
 router.get("/", controllerWrapper(getAllNews));
 
 router.get("/search/:qwery", controllerWrapper(getNewsBySearch));
-//router.get("/search/", controllerWrapper(getNewsBySearch));
 
 module.exports = router;
