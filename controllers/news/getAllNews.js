ce8276457d7e3ab1396c7bfc991bd3d040003ee6
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-30 11:38:28 +0300

    Update getAllNews.js

--- controllers/news/getAllNews.js
+++ controllers/news/getAllNews.js
@@ -3,12 +3,6 @@ const { News } = require("../../schemas/newsModel");
 const getAllNews = async (_, res) => {
   const news = await News.find().sort({ date: -1 });
 
-  if (news.length === 0) {
-    res.json({
-      message: "Not found",
-    });
-  }
-
   res.status(200).json(news);
 };
 
