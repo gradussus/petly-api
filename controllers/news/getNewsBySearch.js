35cfd43e91d8eafc40f2e3bb1aa6d3c591439c42
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-30 11:38:26 +0300

    Update getNewsBySearch.js

--- controllers/news/getNewsBySearch.js
+++ controllers/news/getNewsBySearch.js
@@ -2,17 +2,10 @@ const { News } = require("../../schemas/newsModel");
 
 const getNewsBySearch = async (req, res) => {
   const { qwery } = req.params;
-  //const { qwery } = req.query;
 
   const data = await News.find({
     title: { $regex: qwery, $options: "i" },
   }).sort({ date: -1 });
-
-  if (data.length === 0) {
-    res.json({
-      message: "Not found",
-    });
-  }
 
   res.status(200).json(data);
 };
