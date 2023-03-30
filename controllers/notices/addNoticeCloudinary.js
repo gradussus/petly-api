a7b12ecda27a7845420c28c12847634f05e405f9
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-30 11:38:20 +0300

    Update addNoticeCloudinary.js

--- controllers/notices/addNoticeCloudinary.js
+++ controllers/notices/addNoticeCloudinary.js
@@ -8,22 +8,6 @@ const addNotice = async (req, res) => {
     : { owner, ...noticeData };
 
   const newNotice = await Notice.create(data);
-  // .then((pet) => {
-  //   if (pet) {
-  //     User.findByIdAndUpdate(owner, { $push: { userPets: pet._id } })
-  //       .then((user) => {
-  //         if (user) {
-  //           res.status(201).json({ success: true, pet });
-  //         }
-  //       })
-  //       .catch((err) => {
-  //         throw new Error(err);
-  //       });
-  //   }
-  // })
-  // .catch((err) =>
-  //   res.status(400).json({ success: false, error: err, message: err.message })
-  // );
 
   res.status(201).json(newNotice);
 };
