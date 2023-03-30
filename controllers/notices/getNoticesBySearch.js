a5495862ef3e77f2da8feee285f246cbc565edc2
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-03-30 11:37:25 +0300

    Update getNoticesBySearch.js

--- controllers/notices/getNoticesBySearch.js
+++ controllers/notices/getNoticesBySearch.js
@@ -1,7 +1,5 @@
 const { Notice } = require("../../schemas/noticeModel");
 
-// пошук по слову і категорії
-
 const getNoticesBySearch = async (req, res) => {
   const { categoryName, qwery } = req.params;
 
@@ -12,25 +10,7 @@ const getNoticesBySearch = async (req, res) => {
     createdAt: -1,
   });
 
-  // if (notices.length === 0) {
-  //   res.json({
-  //     message: "Not found",
-  //   });
-  // }
-
   res.status(200).json(notices);
 };
-
-// const getNoticesBySearch = async (req, res) => {
-//   const { qwery } = req.params;
-
-//   const notices = await Notice.find({
-//     title: { $regex: qwery, $options: "i" },
-//   }).sort({
-//     createdAt: -1,
-//   });
-
-//   res.status(200).json(notices);
-// };
 
 module.exports = getNoticesBySearch;
